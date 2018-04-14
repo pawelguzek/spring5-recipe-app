c3999c3c4a741c6d3b6f8267f67df850ed03a5fd
pawelguzek <paw_8@o2.pl>
2018-04-14 23:03:00 +0200

    Added Enum

---
+++ src/main/java/guru/springframework/domain/Difficulty.java
@@ -0,0 +1,12 @@
+package guru.springframework.domain;
+
+import javax.persistence.Entity;
+
+/**
+ * Created by Pawel on 14.04.2018.
+ */
+@Entity
+public enum Difficulty {
+    EASY, MODERATE, HARD
+
+}
