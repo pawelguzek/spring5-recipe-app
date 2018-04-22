3db971993c2758838089feb7a8e1e5f0818961d8
pawelguzek <paw_8@o2.pl>
2018-04-23 00:03:39 +0200

    Added UnitTests

---
+++ src/test/java/guzek/springframework/domain/CategoryTest.java
@@ -0,0 +1,36 @@
+package guzek.springframework.domain;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Pawel on 22.04.2018.
+ */
+public class CategoryTest {
+
+    Category category;
+
+    @Before
+    public void setUp(){
+        category = new Category();
+    }
+
+    @Test
+    public void getId() throws Exception {
+        Long idValue = 4L;
+        category.setId( idValue);
+
+        assertEquals(idValue,category.getId());
+    }
+
+    @Test
+    public void getDescription() throws Exception {
+    }
+
+    @Test
+    public void getRecipes() throws Exception {
+    }
+
+}
---
+++ src/test/java/guzek/springframework/services/RecipeServiceImplTest.java
@@ -0,0 +1,45 @@
+package guzek.springframework.services;
+
+import guzek.springframework.domain.Recipe;
+import guzek.springframework.repositories.RecipeRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
+/**
+ * Created by Pawel on 22.04.2018.
+ */
+public class RecipeServiceImplTest {
+
+    private RecipeServiceImpl recipeService;
+
+    @Mock
+    private RecipeRepository recipeRepository;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        recipeService = new RecipeServiceImpl(recipeRepository);
+    }
+
+    @Test
+    public void getRecipes() throws Exception {
+        Recipe recipe = new Recipe();
+        HashSet receipesData = new HashSet();
+        receipesData.add(recipe);
+
+        when(recipeService.getRecipes()).thenReturn(receipesData);
+
+        Set<Recipe> recipes = recipeService.getRecipes();
+        assertEquals(recipes.size(), 1);
+        verify(recipeRepository, times(1)).findAll();
+    }
+
+}
